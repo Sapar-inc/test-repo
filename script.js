7f0a0d7af29ad87786172c78c2669abb465e2568
Sapar Saparov <you@example.com>
2022-10-18 21:16:17 +0600

    task with folders done

---
+++ script.js
@@ -0,0 +1,39 @@
+let desktop = [
+    {
+        name:"Folder 1",
+        folders: [
+                {name:"SubFolder 1"},
+                {name:"SubFolder 2"},
+                {name:"SubFolder 3"},
+                ]
+    },
+    {
+        name:"Folder 2",
+        folders: [
+                {name:"SubFolder 4"},
+                {name:"SubFolder 5"},
+                {name:"SubFolder 6"},
+                 ]
+    },
+    {
+        name:"Folder 3",
+        folders: [
+                {name:"SubFolder 7"},
+                {name:"SubFolder 8"},
+                {name:"SubFolder 9"},
+                ]
+    },
+]
+
+function showParentFolderName(subfolderName){
+    for(let i = 0; i < desktop.length; i++){
+        for(let k = 0; k < desktop[i].folders.length;k++){
+            if(subfolderName == desktop[i].folders[k].name){
+                console.log(desktop[i].name)
+            }
+    }
+}
+}
+showParentFolderName("SubFolder 9")
+
+// console.log(desktop[0].folders.length)
